883fcdc60d0137a0f77f1a3507850cb341f886d1
George Neres <george.neres100@gmail.com>
2024-07-02 11:38:01 -0300

    somatorio-notas

---
+++ Documents/NovaAula/Somatorio_notas/soma_notas.py
@@ -0,0 +1,5 @@
+nome = str(input('Qual é o seu nome?'))
+idade = int(input('Quantos anos vc tem?'))
+peso = float(input('Qual o seu peso?'))
+
+print(f"O nome do ususario e {nome}", f"Idade {idade}",f"Pesa {peso}")
