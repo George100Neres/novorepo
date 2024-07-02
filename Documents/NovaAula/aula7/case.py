18c473bd2cd7ace949b4cc441883b932dcf2d839
George Neres <george.neres100@gmail.com>
2024-07-02 10:59:36 -0300

    news exercises

---
+++ Documents/NovaAula/aula7/case.py
@@ -0,0 +1,9 @@
+def par_or_impar():
+    number = int(input("Digite um numero: "))
+    if number % 2 == 0:
+        print("VOCE ESTA NO TIME AZUL")
+    else:
+        print("VOCE ESTA NO TIME AMARELO")
+
+# Chamada da função
+par_or_impar()
---
+++ Documents/NovaAula/aula7/msg_ano_novo.py
@@ -0,0 +1,6 @@
+
+
+def msganonovo():
+    print('Adeus Ano velho, feliz ano novo!')
+
+msganonovo()
